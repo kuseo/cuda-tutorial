8a2568e2354a5dfa8e285ee89e538a2a87ae0d03
kanguk Seo <tjrkddnr@korea.ac.kr>
2018-07-19 06:26:32 +0900

    implement gpu add

--- cuda tutorial/cuda tutorial 2/kernel.cu
+++ cuda tutorial/cuda tutorial 2/kernel.cu
@@ -3,6 +3,17 @@
 #include "device_launch_parameters.h"
 #include <book.h>
 #include <stdio.h>
+#define N 10
+
+__global__ void add(int * a, int * b, int * c)
+{
+	int tid = threadIdx.x + blockIdx.x * blockDim.x; //(스레드 인덱스) + (블럭 인덱스) * (한 블럭당 스레드 수)
+	while (tid < N)
+	{
+		c[tid] = a[tid] + b[tid];
+		tid += blockDim.x * gridDim.x;
+	}
+}
 
 int main()
 {
