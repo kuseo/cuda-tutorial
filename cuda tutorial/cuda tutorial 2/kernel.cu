ed43d73e1266b647726c3bec03dd9771a071d482
kanguk Seo <tjrkddnr@korea.ac.kr>
2018-07-19 05:52:30 +0900

    실습 기본 셋팅

--- cuda tutorial/cuda tutorial 2/kernel.cu
+++ cuda tutorial/cuda tutorial 2/kernel.cu
@@ -4,13 +4,6 @@
 #include <book.h>
 #include <stdio.h>
 
-
-__global__ void addKernel(int *c, const int *a, const int *b)
-{
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
-}
-
 int main()
 {
     
