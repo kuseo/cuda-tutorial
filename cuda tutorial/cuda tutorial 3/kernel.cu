0c6072f5db6e4165278ace9a3377bc37de99c79c
kanguk Seo <tjrkddnr@korea.ac.kr>
2018-07-26 06:42:13 +0900

    상수 주석 추가

--- cuda tutorial/cuda tutorial 3/kernel.cu
+++ cuda tutorial/cuda tutorial 3/kernel.cu
@@ -7,9 +7,9 @@
 #include <book.h>
 #define imin(a,b) (a<b?a:b)
 
-const int N = 33 * 1024;
-const int threadsPerBlock = 256;
-const int blocksPergrid = imin(32, (N + threadsPerBlock - 1) / threadsPerBlock);
+const int N = 33 * 1024; //내적할 벡터의 차원
+const int threadsPerBlock = 256; //한 블럭당 스레드 수
+const int blocksPergrid = imin(32, (N + threadsPerBlock - 1) / threadsPerBlock); //블럭 낭비 방지
 
 int main()
 {
