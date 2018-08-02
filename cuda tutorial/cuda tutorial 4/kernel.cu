929e2d810f085239c244b00e610dc2a4bea8ee16
kanguk Seo <tjrkddnr@korea.ac.kr>
2018-08-02 18:38:28 +0900

    bitmap display 기본 세팅

--- cuda tutorial/cuda tutorial 4/kernel.cu
+++ cuda tutorial/cuda tutorial 4/kernel.cu
@@ -3,9 +3,31 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
+#include <book.h>
+#include <cpu_bitmap.h>
+
+#define DIM 1024
+#define PI 3.141592
+
+__global__ void kernel(unsigned char *ptr)
+{
+	
+
+}
 
 int main()
 {
-    
+	CPUBitmap bitmap(DIM, DIM);			//1024 * 1024 비트
+	unsigned char *dev_bitmap;
+
+	HANDLE_ERROR(cudaMalloc((void**)&dev_bitmap, bitmap.image_size()));
+	dim3 grids(DIM / 16, DIM / 16);		//16 * 16 블럭
+	dim3 threads(16, 16);				//
+	//kernel << <grids, threads >> > (dev_bitmap);
+	HANDLE_ERROR(cudaMemcpy(bitmap.get_bitmap_ptr(), dev_bitmap, bitmap.image_size(), cudaMemcpyDeviceToHost)));
+
+	bitmap.display_and_exit();
+	cudaFree(dev_bitmap);
+
     return 0;
 }
