d1d1f656f24eaa80c2eff9bda87815929ce4720b
kanguk Seo <tjrkddnr@korea.ac.kr>
2018-08-11 05:53:33 +0900

    Sphere 구조체 정의 및 선언

--- cuda tutorial/cuda tutorial 5/kernel.cu
+++ cuda tutorial/cuda tutorial 5/kernel.cu
@@ -2,11 +2,41 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-#include <stdio.h>
+#include <cmath>
+#include <cpu_bitmap.h>
+#include <book.h>
+
+#define INF 2e10f
+
+struct Sphere
+{
+	/*
+	구의 속성값
+	*/
+	float r, g, b;
+	float radius;
+	float x, y, z;
+
+	/*
+	projective view
+	(ox, oy)에 위치한 픽셀에서 발사한 광선이 구와 충돌하는지 판별. 충돌시 그 깊이값 반환 
+	*/
+	__device__ float hit(float ox, float oy, float *n)
+	{
+		float dx = ox - x;
+		float dy = oy - y;
+		if (radius * radius > dx*dx + dy*dy)
+		{
+			float dz = sqrtf(radius*radius - dx * dx - dy * dy);
+			*n = dz / sqrtf(radius*radius); //음영을 부여하기 위한 scale 값. 구의 중심에서 멀어질수록 작아짐
+			return dz + z; //깊이값을 반환
+		}
+		return -INF;
+	}
+};
 
 int main()
 {
     
     return 0;
 }
-
