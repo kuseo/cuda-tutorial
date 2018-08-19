b7d2b23b43352ba3dc471545d7dbcfb725c79ed1
kanguk Seo <tjrkddnr@korea.ac.kr>
2018-08-20 03:41:23 +0900

    openGL CUDA interoperate setting

--- cuda tutorial/cuda tutorial 6/kernel.cu
+++ cuda tutorial/cuda tutorial 6/kernel.cu
@@ -3,10 +3,39 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
+#include <GL/glut.h>
+#include <cuda_gl_interop.h>
+#include <book.h>
+#include <cpu_bitmap.h>
+#define DIM 512
 
-int main()
+/*
+공유 데이터 핸들
+*/
+GLuint bufferObj;
+cudaGraphicsResource *resource;
+
+int main(int argc, char **argv)
 {
-   
+	cudaDeviceProp prop;	//cuda device
+	int dev;	//cuda device 식별값
+	
+	/*
+	cuda device 멤버 값을 0으로 초기화한 후 주 버전을 1, 부 버전을 0으로 설정
+	*/
+	memset(&prop, 0, sizeof(cudaDeviceProp));
+	prop.major = 1;
+	prop.minor = 0;
+	
 
+	HANDLE_ERROR(cudaChooseDevice(&dev, &prop));
+	HANDLE_ERROR(cudaGLSetGLDevice(dev));
+
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE);
+	glutInitWindowSize(DIM, DIM);
+	glutCreateWindow("bitmap");
+
+	//glutMainLoop();
     return 0;
 }
